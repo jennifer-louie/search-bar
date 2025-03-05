a4cf35951eee7a2d774d469c8b42d41b46755620
jennifer-louie <jenniferlouie@icloud.com>
2025-03-04 21:31:45 -0800

    Add search functionality and UI

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [value, setValue] = useState(""); // search value
+  const [data, setData] = useState([]); // API data
+  
+  const onChange = async (e) => {
+    setValue(e.target.value); 
+    const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+    const responseData = await response.json(); 
+    setData(responseData); 
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="search-box">
+        <div className="row">
+          <input type="text" onChange={onChange} value={value} placeholder="Search" />
+          <button><i class="fa-solid fa-magnifying-glass"></i></button>
+        </div>
+        <div className="result-box">
+          {
+            value && 
+            data.filter(d => d.title.startsWith(value) && d.title !== value)
+              .slice(0, 5)
+              .map(item => <ul>
+                <li key={item.id} onClick={(e) => setValue(item.title)}>
+                  {item.title}
+                </li>
+                </ul>
+              )
+          }
+        </div>
+      </div>
     </div>
   );
 }
